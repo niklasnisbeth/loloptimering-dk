19e13ac31ecfa545921b397484d9b41add9e2203
Niklas Nisbeth <niklas@nisbeth.dk>
2017-03-29 20:08:51 +0200

    loloptimering

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import "net/http"
+import "io/ioutil"
+import "log"
+
+func main() {
+  filePaths := []string{"avokade.jpg", "spacer.bmp"};
+  
+  mkHandler := func (s string) func(w http.ResponseWriter, r *http.Request) {
+    file,_ := ioutil.ReadFile(s);
+    return func(w http.ResponseWriter, r *http.Request) {
+      w.Write(file)
+    }
+  }
+
+  http.HandleFunc("/", mkHandler("index.html"));
+
+  for i, _ := range filePaths {
+    http.HandleFunc("/" + filePaths[i], mkHandler(filePaths[i]));
+  }
+
+  log.Fatal(http.ListenAndServe(":8889", nil))
+}
